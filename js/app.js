a7dd0bd57515260d45b7792a6978ab65da795d4d
Sebastian Palewski <spalewski@gmail.com>
2020-07-27 19:58:17 +0200

    style added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-const result = document.querySelector('#calculated');
 const selectElement = document.querySelector('#currencySelector');
+let valueToCalculate = document.querySelector('#valueToCalculate');
 
-let currenciesTable = 'http://api.nbp.pl/api/exchangerates/tables/a/?format=json';
-fetch(currenciesTable, {
-    method: "get"
-}).then(response => response.json()).then(json => console.log(json[0].rates))
-
-selectElement.addEventListener('change', (event) => {
-    let url = "http://api.nbp.pl/api/exchangerates/rates/a/" + event.target.value + "/?format=json"
+function calculateCurrency() {
+    var selectedCurrency = selectElement.options[selectElement.selectedIndex].value;
+    let url = "http://api.nbp.pl/api/exchangerates/rates/a/" + selectedCurrency + "/?format=json"
     fetch(url, {
         method: "get"
-    }).then(response => response.json()).then(json =>
-        result.textContent = document.querySelector('#valueToCalculate').value * json.rates[0].mid).catch((error) => {
+    }).then(response => response.json()).then(json => {
+        document.querySelector('.calculated').innerHTML = valueToCalculate.value
+            + ' ' + selectedCurrency.toUpperCase() + ' to '
+            + (valueToCalculate.value * json.rates[0].mid) + ' PLN'
+    }).catch((error) => {
         console.error('Error:', error);
     });
-});
+}
